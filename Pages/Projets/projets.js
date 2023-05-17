6c369bdc7f17092f3dcea79cc8aec9d8e05c85d8
YanisRff <113534446+YanisRff@users.noreply.github.com>
2023-05-17 14:33:53 +0200

    almost done with the the up arrow

--- Pages/Projets/projets.js
+++ Pages/Projets/projets.js
@@ -1,12 +1,1 @@
 
-function upArrow() {
-    console.log("upArrow");
-    window.scrollTo(0, 0);
-}
-
-function main() {
-    let upArrowButton = document.getElementById("upArrowButton");
-    upArrowButton.addEventListener("click", upArrow);
-}
-
-main();
