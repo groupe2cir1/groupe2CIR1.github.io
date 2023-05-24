1e568e12486e15dff9a91d97ea660121eb71e96e
YanisRff <113534446+YanisRff@users.noreply.github.com>
2023-05-24 14:14:12 +0200

    essaie de blackCard photo ayoub

---
+++ Pages/Membres/Membres.js
@@ -0,0 +1,9 @@
+function blackCard() {
+    console.log("execute order 66");
+    let card = document.getElementsByClassName("card card0");
+    console.log("hello");
+    card.style.background = "black";
+    console.log("good");
+}
+
+blackCard();
