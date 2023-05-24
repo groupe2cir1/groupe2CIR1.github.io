fa1885a22ccd5a505c625d1820d2ec105ea74e9e
YanisRff <113534446+YanisRff@users.noreply.github.com>
2023-05-24 15:31:39 +0200

    membres suite

--- Pages/Membres/Membres.js
+++ Pages/Membres/Membres.js
@@ -112,4 +112,26 @@ function blackCard() {
     
 }
 
-blackCard();
+function modalCard() {
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function() {
+        modal.style.display = "block";
+      }
+      span.onclick = function() {
+        modal.style.display = "none";
+      }
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+    }
+}
+
+function main () {
+    blackCard();
+
+}
+
+main();
