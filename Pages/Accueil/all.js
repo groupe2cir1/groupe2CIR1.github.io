81b0710177b6084a05f5df730ff2160ea5909662
Nirij3m <nirina.macon@isen-ouest.yncrea.fr>
2023-05-27 19:29:43 +0200

    Rework Footer et Intégration
    Coup de neuf pour le footer et intégration d'une fonction d'appel

--- Pages/Accueil/all.js
+++ Pages/Accueil/all.js
@@ -1,3 +1,78 @@
+function openFooterModal(){
+
+    let modal = document.getElementById("myModal");
+    let btn = document.getElementById("myBtn");
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            document.getElementById("modalContainer").style.display = "none";
+            document.getElementById("num").value = "";
+        }
+    }
+
+}
+
+function wannaCall(element){
+    document.getElementById("modalContainer").style.display = "block";
+    let phoneNumber = element.innerText;
+    let audio = new Audio('../../cahier\ des\ charges/ressources/saulringtone.mp3');
+    let callButton = document.getElementById("call");
+    let prompt = document.getElementById("prompt");
+    let callText = document.createElement("p");
+    callText.innerText = "Appel en cours...";
+
+    document.getElementById("call").addEventListener("click", function(){ //j'ai cliqué sur décrocher
+        if(document.getElementById("num").value.replace(/\s/g, "") === phoneNumber.replace(/\+33|\(|\)|\s/g, "")){ //compare le num rentré avec le num de la div sans les espaces
+
+            prompt.append(callText);
+            callText.className = "tracking-in-expand";
+
+
+            callButton.classList.add("wobble-hor-bottom");
+            audio.play(); //lance l'audio
+            setTimeout(function(){ //le coupe au bout de 11 secondes
+                audio.pause();
+                audio.currentTime = 0; // réinitialise l'audio
+                callButton.classList.remove("wobble-hor-bottom");
+                prompt.removeChild(prompt.lastChild);
+            }, 11000);
+        }
+        else{
+            alert("Le numéro entré est incorrect");
+            document.getElementById("num").value = "";
+            callButton.classList.add("blink-1");
+            setTimeout(function(){
+                callButton.classList.remove("blink-1");
+            }, 1000);
+
+        }
+    });
+
+    document.getElementById("uncall").addEventListener("click", function() { //j'ai cliqué sur raccrocher
+        audio.pause();
+        audio.currentTime = 0;
+        document.getElementById("modalContainer").style.display = "none";
+        document.getElementById("num").value = "";
+        prompt.removeChild(prompt.lastChild);
+
+    });
+}
+
+
+function telOnCopy(){
+    Array.from(document.getElementsByClassName("tel")).forEach(function(element) {
+        element.addEventListener('copy', function(){
+            wannaCall(element);
+        });
+    });
+}
+
+
 
 function copyAlert() {
     Array.from(document.getElementsByTagName('p')).forEach(element => {
@@ -57,5 +132,7 @@ function main() {
     copyAlert();
     returnOnClick();
     redirectOnClick();
+    openFooterModal();
+    telOnCopy();
 }
 main();
--- Pages/HeaderFooter/newHeaderFooter.js
+++ Pages/HeaderFooter/newHeaderFooter.js
@@ -1,29 +1,79 @@
+function openFooterModal(){
 
-function main(){
-// Get the modal
-    var modal = document.getElementById("myModal");
+    let modal = document.getElementById("myModal");
+    let btn = document.getElementById("myBtn");
 
-// Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
     btn.onclick = function() {
         modal.style.display = "block";
     }
 
-// When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-// When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
+            document.getElementById("modalContainer").style.display = "none";
+            document.getElementById("num").value = "";
         }
     }
+
+}
+
+function wannaCall(element){
+    document.getElementById("modalContainer").style.display = "block";
+    let phoneNumber = element.innerText;
+    let audio = new Audio('../../cahier\ des\ charges/ressources/saulringtone.mp3');
+    let callButton = document.getElementById("call");
+    let prompt = document.getElementById("prompt");
+    let callText = document.createElement("p");
+    callText.innerText = "Appel en cours...";
+
+    document.getElementById("call").addEventListener("click", function(){ //j'ai cliqué sur décrocher
+        if(document.getElementById("num").value.replace(/\s/g, "") === phoneNumber.replace(/\+33|\(|\)|\s/g, "")){ //compare le num rentré avec le num de la div sans les espaces
+
+            prompt.append(callText);
+            callText.className = "tracking-in-expand";
+
+
+            callButton.classList.add("wobble-hor-bottom");
+            audio.play(); //lance l'audio
+            setTimeout(function(){ //le coupe au bout de 11 secondes
+                audio.pause();
+                audio.currentTime = 0; // réinitialise l'audio
+                callButton.classList.remove("wobble-hor-bottom");
+                prompt.removeChild(prompt.lastChild);
+            }, 11000);
+        }
+        else{
+            alert("Le numéro entré est incorrect");
+            document.getElementById("num").value = "";
+            callButton.classList.add("blink-1");
+            setTimeout(function(){
+                callButton.classList.remove("blink-1");
+            }, 1000);
+
+        }
+    });
+
+    document.getElementById("uncall").addEventListener("click", function() { //j'ai cliqué sur raccrocher
+        audio.pause();
+        audio.currentTime = 0;
+        document.getElementById("modalContainer").style.display = "none";
+        document.getElementById("num").value = "";
+        prompt.removeChild(prompt.lastChild);
+
+    });
+}
+
+
+function telOnCopy(){
+    Array.from(document.getElementsByClassName("tel")).forEach(function(element) {
+            element.addEventListener('copy', function(){
+                wannaCall(element);
+            });
+        });
+    }
+
+function main(){
+    openFooterModal();
+    telOnCopy();
 }
 main();
